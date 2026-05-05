674252b039b3a6a3707ae0356cfa1d59158711f7
Pre-Mystdwyse <mystdwise@gmail.com>
2026-05-05 18:42:57 +0300

    day-13 start optimizating js

---
+++ day-13/script.js
@@ -0,0 +1,9 @@
+const hero = {
+    name: 'Карбел',
+    gold: 10000,
+    inventory: []
+};
+
+document.querySelector('#gold-count') = hero.gold;
+
+const btn = document.querySelectorAll('.btn-trade');
